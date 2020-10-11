72ff7b14e7007793cd92239dde221496ea895929
XivC <57796626+XivC@users.noreply.github.com>
2020-10-11 20:32:55 +0300

    Add files via upload

---
+++ script.sh
@@ -0,0 +1,53 @@
+#! /bin/sh
+echo Задание 1
+mkdir geodude3 gothita1 togekiss1
+cd geodude3
+mkdir simisage houndour vileplume
+cd ..
+cd gothita1
+mkdir roselia growlithe garbodor gigalith
+echo "Способности Overgrow Sand Veil Rough\nSkin" > cacnea
+echo "Способности Overcharge Static\nLightningrod" > manectric
+cd ..
+cd togekiss1
+mkdir igglybuff electrike metagross
+cd ..
+
+echo "Ходы Body Slam Defense Curl Double-Edge Fire\nPunch Helping Hand Hyper Voice Ice Punch Icy Wind Magic Coat Magic\nRoom Mud-Slap Pain Split Recycle Role Play Shock Wave Signal Beam\nSkill Swap Sleep Talk Snatch Snore Swift Thunderpunch Trick Wonder\nRoom Zen Headbutt" > ralts1
+echo "Тип покемона NORMAL NONE" > snorlax7
+echo "Тип\nпокемона GRASS POISON" > vileplume3
+echo Задание 2
+chmod 375 geodude3
+chmod 307 geodude3/simisage
+chmod 764 geodude3/houndour
+chmod 311 geodude3/vileplume
+chmod 333 gothita1
+chmod 600 gothita1/cacnea
+chmod 513 gothita1/roselia
+chmod 400 gothita1/manectric
+chmod 752 gothita1/growlithe
+chmod 750 gothita1/garbodor
+chmod 700 gothita1/gigalith
+chmod 630 ralts1
+chmod 404 snorlax7
+chmod 305 togekiss1
+chmod 363 togekiss1/igglybuff
+chmod 524 togekiss1/electrike
+chmod 524 togekiss1/electrike
+chmod 444 vileplume3
+echo Задание 3
+ln -s `pwd`/snorlax7 `pwd`/gothita1/cacneasnorlax
+cat gothita1/manectric gothita1/manectric > vileplume3_74
+cp snorlax7 togekiss1/igglybuff
+ln vileplume3 gothita1/cacneavileplume
+ln -s gothita1 Copy_31
+chmod 777 gothita1
+cp -r gothita1 togekiss1/igglybuff
+chmod 333 gothita1
+cat ralts1 > gothita1/manectricralts
+
+
+
+
+
+
